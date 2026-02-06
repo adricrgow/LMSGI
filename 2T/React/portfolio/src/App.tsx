cb66f1f2354c7095c1797fff4a33e85f01ac2b9b
adricrgow <adricrgow07@gmail.com>
2026-02-06 12:38:43 +0100

    Dia06/02/2026

--- 2T/React/portfolio/src/App.tsx
+++ 2T/React/portfolio/src/App.tsx
@@ -1,34 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// Importa el estilo css de este componente
 import './App.css'
 
+// Definicion de componente
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
+
+return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React Mi primer proyecto react</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <h1 className='rounded-lg bg-amber-700 p-8'>Mi primera web con React JS</h1>
+      
+      
+    <button type="button" className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-base text-sm px-4 py-2.5 text-center leading-5">Blue</button>
+    <button type="button" className="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-base text-sm px-4 py-2.5 text-center leading-5">Green</button>
+    <button type="button" className="text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-base text-sm px-4 py-2.5 text-center leading-5">Cyan</button>
+    <button type="button" className="text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 font-medium rounded-base text-sm px-4 py-2.5 text-center leading-5">Teal</button>
+    <button type="button" className="text-heading bg-gradient-to-r from-lime-200 via-lime-400 to-lime-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-lime-300 dark:focus:ring-lime-800 font-medium rounded-base text-sm px-4 py-2.5 text-center leading-5">Lime</button>
+    <button type="button" className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-base text-sm px-4 py-2.5 text-center leading-5">Red</button>
+    <button type="button" className="text-white bg-gradient-to-r from-pink-400 via-pink-500 to-pink-60  hover:bg-gradient-to-br focus:ring=focus:outline-none focus:ring-pink=dark:focus:pink=font-medium rounded-base text-sm px=py=text-center leading=5">Pink</button>
+    <button type="button" className="text-white bg-gradient-to-r from-purple-.via-purple-.to-purple-.hover:bg-gradient-to-br.focus-ring-.focus-outline-none.focus-ring-purple-.dark-focus-ring-purple-.font-medium.rounded-base.text-sm.px-.py-.text-center.leading=5">Purple</button>
+
+    
     </>
+
+      
+
   )
 }
 
--- 2T/React/portfolio/src/main.tsx
+++ 2T/React/portfolio/src/main.tsx
@@ -5,7 +5,9 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    {/* Este componete viene de la exportación del App.tsx */}
+    {/* Este componete viene de la exportación del App.tsx 
+    Todo el html que devuelver el app se sustituye en  la etiqueta <App />
+    */}
     <App />
   </StrictMode>,
 )
