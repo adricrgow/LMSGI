5ce56ac6b8b648fa672888b7f4028d5a297cd8a0
adricrgow <adricrgow07@gmail.com>
2026-02-17 09:06:34 +0100

    Martes 17/02

---
+++ 2T/React/portfolio/src/layout/MainLayout.tsx
@@ -0,0 +1,12 @@
+// este componente crea el frontend de la aplicación
+
+import Header from "../components/main/Header";
+
+// La parte publica de la aplicación
+function MainLayout(){
+    return(
+        <Header/>
+    )
+}
+
+export default MainLayout;
