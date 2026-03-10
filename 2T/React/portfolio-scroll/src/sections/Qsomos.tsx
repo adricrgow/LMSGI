dd89fa0446b9e7abce7787ab0fec2da0cfe90747
adricrgow <adricrgow07@gmail.com>
2026-03-10 11:33:04 +0100

    Intalacion iconos Dia 10/03/2026

--- 2T/React/portfolio-scroll/src/sections/Qsomos.tsx
+++ 2T/React/portfolio-scroll/src/sections/Qsomos.tsx
@@ -1,6 +1,7 @@
 // forma tradicional de definir los componentes en React
 
 // function Qsomos() {
+import { Button } from "@/components/ui/button"
 //     return ()
 // }
 
@@ -17,7 +18,9 @@ const Qsomos = () => {
     return (
         <section id="qsomos"className="main-h-screen flex items-center justify-center">
             <h1 className="text-4xl font-bold">Quienes somos</h1>
+        <Button variant="bluex">Quienes somos</Button>
         </section>
+        
     )
 }
 export default Qsomos;
